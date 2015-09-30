94cf5f18a59ac850ac7e9378ec9dd508d8e21a1c
Emerson Borges <emerson2borges@gmail.com>
2015-09-30 15:06:51 -0300

    Atualização do carnê
    Atualização dos códigos do carnê.
    Falta saída

--- src/BoletosImplementados/WebFormCarnet.aspx.cs
+++ src/BoletosImplementados/WebFormCarnet.aspx.cs
@@ -13,22 +13,16 @@ namespace BoletoCAIXA
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            List<List<String>> listaAuxiliar = new List<List<String>>();
-            List<String> minhaLista1 = new List<String>();
-            List<String> minhaLista2 = new List<String>();
-            List<String> minhaLista3 = new List<String>();
+            List<BoletoBancario> listaAuxiliar = new List<BoletoBancario>();
             
             //Dados dos boletos
             short codigoDoBanco = 104;
 
+            //Configura as datas do boleto
             DateTime dataDoDocumeto        = DateTime.Now;
             String dataDoDocumentoString   = dataDoDocumeto.ToString("dd/MM/YYYY");
-            //DateTime dt                    = DateTime.ParseExact("24/01/2013", "dd/MM/yyyy", CultureInfo.InvariantCulture);
-            //DateTime dataDoDocumentoString = Convert.ToDateTime("27/11/2007");
-
             DateTime dataDeVencimento     = DateTime.Now.AddDays(5);
             String dataDoVencimentoString = dataDoDocumeto.ToString("dd-MM-YYYY");
-
             DateTime dataDoProcessamento     = DateTime.Now;
             String dataDoProcessamentoString = dataDoDocumeto.ToString("dd-MM-YYYY");
 
@@ -60,129 +54,53 @@ namespace BoletoCAIXA
 
             int numeroDeParcelas = 2;
             String numeroDeParcelasString = Convert.ToString(numeroDeParcelas);
-
-
-            //Cria os dados do carne 1 
-            minhaLista1.Add(dataDoDocumentoString);
-            minhaLista1.Add(dataDoVencimentoString);
-            minhaLista1.Add(dataDoProcessamentoString);
-            minhaLista1.Add(cnpjDoCedente);
-            minhaLista1.Add(nomeDoCedente);
-            minhaLista1.Add(agenciaDoCedente);
-            minhaLista1.Add(contaDoCedente);
-            minhaLista1.Add(digitoDaConta);
-            minhaLista1.Add(valorDoCarneString);
-            minhaLista1.Add(carteira);
-            minhaLista1.Add(nossoNumero);
-            minhaLista1.Add(cpfDoSacado);
-            minhaLista1.Add(nomeDoSacado);
-            minhaLista1.Add(ruaDoSacado);
-            minhaLista1.Add(bairroDoSacado);
-            minhaLista1.Add(cidadeDoSacado);
-            minhaLista1.Add(cepDoSacado);
-            minhaLista1.Add(estadoDoSacado);
-            minhaLista1.Add(numeroDocumento);
-
-            //Cria os dados do carne 2
-            minhaLista2.Add(dataDoDocumentoString);
-            minhaLista2.Add(dataDoVencimentoString);
-            minhaLista2.Add(dataDoProcessamentoString);
-            minhaLista2.Add(cnpjDoCedente);
-            minhaLista2.Add(nomeDoCedente);
-            minhaLista2.Add(agenciaDoCedente);
-            minhaLista2.Add(contaDoCedente);
-            minhaLista2.Add(digitoDaConta);
-            minhaLista2.Add(valorDoCarneString);
-            minhaLista2.Add(carteira);
-            minhaLista2.Add(nossoNumero);
-            minhaLista2.Add(cpfDoSacado);
-            minhaLista2.Add(nomeDoSacado);
-            minhaLista2.Add(ruaDoSacado);
-            minhaLista2.Add(bairroDoSacado);
-            minhaLista2.Add(cidadeDoSacado);
-            minhaLista2.Add(cepDoSacado);
-            minhaLista2.Add(estadoDoSacado);
-            minhaLista2.Add(numeroDocumento);
-
-            //Cria os dados do carne 2
-            minhaLista3.Add(dataDoDocumentoString);
-            minhaLista3.Add(dataDoVencimentoString);
-            minhaLista3.Add(dataDoProcessamentoString);
-            minhaLista3.Add(cnpjDoCedente);
-            minhaLista3.Add(nomeDoCedente);
-            minhaLista3.Add(agenciaDoCedente);
-            minhaLista3.Add(contaDoCedente);
-            minhaLista3.Add(digitoDaConta);
-            minhaLista3.Add(valorDoCarneString);
-            minhaLista3.Add(carteira);
-            minhaLista3.Add(nossoNumero);
-            minhaLista3.Add(cpfDoSacado);
-            minhaLista3.Add(nomeDoSacado);
-            minhaLista3.Add(ruaDoSacado);
-            minhaLista3.Add(bairroDoSacado);
-            minhaLista3.Add(cidadeDoSacado);
-            minhaLista3.Add(cepDoSacado);
-            minhaLista3.Add(estadoDoSacado);
-            minhaLista3.Add(numeroDocumento);
-
-            //Adiciona as listas com os dados na lista principal
-            listaAuxiliar.Add(minhaLista1);
-            listaAuxiliar.Add(minhaLista2);
-            listaAuxiliar.Add(minhaLista3);
             
+            //Cria boleto
+            BoletoBancario boletoBancario = new BoletoBancario();
+            boletoBancario.CodigoBanco = codigoDoBanco;
+            
+            //Cria cedente
+            Cedente c = new Cedente(cnpjDoCedente, nomeDoCedente, agenciaDoCedente, contaDoCedente, digitoDaConta);
+            c.Codigo = codigoDoCedente;
 
-            for(int i=0; i<listaAuxiliar.Count; i++)
+            Boleto boleto = new Boleto(dataDeVencimento, valorDoCarne, carteira, nossoNumero, c);
+            boleto.DataDocumento = dataDoDocumeto;
+            boleto.DataProcessamento = dataDoProcessamento;
+
+            boleto.Sacado = new Sacado(cpfDoSacado, nomeDoSacado);
+            boleto.Sacado.Endereco.End = ruaDoSacado;
+            boleto.Sacado.Endereco.Bairro = bairroDoSacado;
+            boleto.Sacado.Endereco.Cidade = cidadeDoSacado;
+            boleto.Sacado.Endereco.CEP = cepDoSacado;
+            boleto.Sacado.Endereco.UF = estadoDoSacado;
+
+            EspecieDocumento_Caixa espDocCaixa = new EspecieDocumento_Caixa();
+            boleto.EspecieDocumento = new EspecieDocumento_Caixa(espDocCaixa.getCodigoEspecieByEnum(EnumEspecieDocumento_Caixa.DuplicataMercantil));
+
+            boleto.NumeroDocumento = numeroDocumento;
+            boleto.DataProcessamento = dataDoProcessamento;
+            boleto.DataDocumento = dataDoDocumeto;
+
+            boletoBancario.Boleto = boleto;
+            boletoBancario.MostrarComprovanteEntrega = true;
+            boletoBancario.FormatoCarne = true;
+            
+            boletoBancario.Boleto.Valida();
+
+            for (int i=0; i<6; i++)
             {
-                //Cria boleto
-                BoletoBancario boletoBancario = new BoletoBancario();
-                boletoBancario.CodigoBanco = codigoDoBanco;
+                listaAuxiliar.Add(boletoBancario);
+                
+            }
+
+            for(int i=0; i < listaAuxiliar.Count; i++)
+            {
+                Panel1.Controls.Add(listaAuxiliar[i]);
+            }
+
+            Label1.Text = listaAuxiliar.Count.ToString();
             
-                //TODO: Iniciar a criacao dos carnes a partir dos dados da lista auxiliar
-                Cedente c = new Cedente(cnpjDoCedente, nomeDoCedente, agenciaDoCedente, contaDoCedente,digitoDaConta);
 
-                c.Codigo = codigoDoCedente;
-
-                Boleto boleto = new Boleto(dataDeVencimento, valorDoCarne, carteira, nossoNumero, c);
-                boleto.DataDocumento = dataDoDocumeto;
-                boleto.DataProcessamento = dataDoProcessamento;
-
-                boleto.Sacado = new Sacado(cpfDoSacado, nomeDoSacado);
-                boleto.Sacado.Endereco.End = ruaDoSacado;
-                boleto.Sacado.Endereco.Bairro = bairroDoSacado;
-                boleto.Sacado.Endereco.Cidade = cidadeDoSacado;
-                boleto.Sacado.Endereco.CEP = cepDoSacado;
-                boleto.Sacado.Endereco.UF = estadoDoSacado;
-
-                //Adiciona as instruções ao boleto
-                /*
-                #region Instruções
-                        Instrucao_Caixa item;
-                        //ImportanciaporDiaDesconto
-                        item = new Instrucao_Caixa((int)EnumInstrucoes_Caixa.Multa, Convert.ToDecimal(0.40));
-                        boleto.Instrucoes.Add(item);
-                        item = new Instrucao_Caixa((int)EnumInstrucoes_Caixa.JurosdeMora, Convert.ToDecimal(0.01));
-                        boleto.Instrucoes.Add(item);
-                        item = new Instrucao_Caixa((int)EnumInstrucoes_Caixa.NaoReceberAposNDias, 90);
-                        boleto.Instrucoes.Add(item);
-                        #endregion Instruções
-                */
-            
-                EspecieDocumento_Caixa espDocCaixa = new EspecieDocumento_Caixa();
-                boleto.EspecieDocumento = new EspecieDocumento_Caixa(espDocCaixa.getCodigoEspecieByEnum(EnumEspecieDocumento_Caixa.DuplicataMercantil));
-                
-                boleto.NumeroDocumento = numeroDocumento;
-                boleto.DataProcessamento = dataDoProcessamento;
-                boleto.DataDocumento = dataDoDocumeto;
-                boletoBancario.Boleto = boleto; 
-                boletoBancario.MostrarComprovanteEntrega = true;
-                boletoBancario.FormatoCarne = true;
-                
-                string montaHTML = boletoBancario.MontaHtml();
-
-                boletoBancario.Boleto.Valida();
-                Panel2.Controls.Add(boletoBancario);
-                
-            }//fim do for (i)
         }
             //Literal1.Text = htmlBoleto;
             
--- src/BoletosImplementados/WebFormCarnet.aspx.designer.cs
+++ src/BoletosImplementados/WebFormCarnet.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace BoletoCAIXA {
     public partial class WebFormCarnet {
         
         /// <summary>
-        /// Panel2 control.
+        /// Panel1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel Panel2;
+        protected global::System.Web.UI.WebControls.Panel Panel1;
         
         /// <summary>
         /// Literal1 control.
@@ -29,5 +29,14 @@ namespace BoletoCAIXA {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Literal Literal1;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
